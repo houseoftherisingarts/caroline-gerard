cf9d7d9d80cd5b097993b4250d93aa6032e9b2b5
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-06 06:30:59 -0400

    autosave: 2026-09-06 06:30:59

--- pages/Admin/AdminLayout.tsx
+++ pages/Admin/AdminLayout.tsx
@@ -120,6 +120,7 @@ const AdminLayout = ({ children }: { children?: React.ReactNode }) => {
 
         <nav className="flex-1 py-4 px-3 space-y-1 overflow-y-auto">
           <NavLink to="/admin" icon={<LayoutDashboard />} label="Tableau de bord" onClick={closeSidebar} />
+          <NavLink to="/admin/demande-changement" icon={<Send />} label="Demander un changement" onClick={closeSidebar} />
           <NavLink to="/admin/commandes" icon={<ShoppingCart />} label="Commandes" onClick={closeSidebar} />
           <NavLink to="/admin/factures" icon={<FileText />} label="Factures & Devis" onClick={closeSidebar} />
           <NavLink to="/admin/echelle" icon={<Layers />} label="Échelle Produits" onClick={closeSidebar} />
