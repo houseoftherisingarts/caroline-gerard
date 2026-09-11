48d14c23fa5d290c8c070810ade158bb53b672f2
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-11 00:53:33 -0400

    autosave: 2026-09-11 00:53:32

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -15,7 +15,7 @@ import {
 } from 'firebase/firestore';
 import { httpsCallable } from 'firebase/functions';
 import { db, functions } from '../firebase';
-import { BlogPost, NewsItem, AppEvent, Conference, Interview, Lead, Subscriber, Member, CommunityMessage, Book, PromoCode, EmailLog, Testimonial, ConsignmentLocation, ConsignmentMovement, StockMovement } from '../types';
+import { BlogPost, NewsItem, AppEvent, Conference, Interview, Lead, Subscriber, Member, CommunityMessage, Book, PromoCode, EmailLog, Testimonial, ConsignmentLocation, ConsignmentMovement, StockMovement, RendezVous, Occupation, StatutRendezVous } from '../types';
 
 // --- Generic helper ---
 
