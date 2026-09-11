1fd63d1549affbe4cae17e1d91d24f5046d7720f
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-11 00:57:41 -0400

    autosave: 2026-09-11 00:57:41

--- pages/CommunautePage.tsx
+++ pages/CommunautePage.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import {
   signInWithPopup,
